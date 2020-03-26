939d034e389070b4c1965bf4a059560789f46e18
hectorbliss <contacto@fixter.org>
2020-03-26 02:04:59 -0600

    hand made react added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-import { example } from './example.js';
+//import { example } from './example.js';
+import home from './components/Home.js'
+import login from './components/Login.js'
 
-example();
+function navigate(path) {
+    root.lastElementChild.remove()
+    render(path)
+}
+
+// nav bar ??
+
+// main 
+function render(path) {
+    let route
+    switch (path) {
+        case '/login':
+            route = login({ document, navigate })
+            break;
+        default:
+            route = home({ document, navigate })
+            break;
+    }
+    root.appendChild(route)
+}
+
+render()
