8b00d9a4b5b45f46a316ee6dea36b6a05f8ba038
hectorbliss <contacto@fixter.org>
2020-04-01 16:32:42 -0600

    login basic structure

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -10,3 +10,29 @@ const firebaseConfig = {
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+let db = firebase.firestore()
+let usersRef = db.collection('users')
+
+export default firebase
+
+export function loginWithGoogle() {
+    let provider = new firebase.auth.GoogleAuthProvider();
+    return firebase.auth().signInWithPopup(provider)
+        .then(snap => {
+            saveUser(snap.user)
+            return snap.user
+        })
+}
+
+function saveUser(user) {
+    let { displayName, uid, photoURL, email } = user
+    let u = {
+        displayName,
+        uid,
+        photoURL,
+        email
+    }
+    usersRef.doc(user.uid).set(u)
+    // local
+    localStorage.user = JSON.stringify(u)
+}
