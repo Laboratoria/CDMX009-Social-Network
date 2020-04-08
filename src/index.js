5354ed3bfd8c021f9762a6c9a352f2204f496bb5
danleonca <danleon022688@gmail.com>
2020-04-08 13:06:55 -0500

    adding fire base and first page

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-import { example } from './example.js';
+//import { example } from './example.js';
+//
+//example();
+//import {firebase} from '@firebase/app';
+//require('firebase/auth');
+let createNewuser= document.getElementById('createUserNw');
+createNewuser.addEventListener('click', () => {
+  console.log('boton que funciona');
+    let emailNew= document.getElementById('emailNw').value;
+    let password= document.getElementById('passwordNw').value;
 
-example();
+    firebase.auth().createUserWithEmailAndPassword(emailNew, password).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorCode);
+        console.log(errorMessage);
+        
+        // ...
+      });
+});
+
+let userNew= document.getElementById('newUser');
+userNew.addEventListener('click', () => {
+  document.getElementById('createUser').style.display = 'block';
+});
