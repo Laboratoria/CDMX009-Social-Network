93ecf75fa77c098da6925479ef2aa35507452956
IrisFyD <iriis.trejo.c@gmail.com>
2020-04-14 02:05:30 -0500

    View login tags and style

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import { changeView } from './viewControler/viewControler.js';
-
+//Load view
 const init = () => {
     changeView(window.location.hash)
     window.addEventListener('hashchange', () => changeView(window.location.hash))
     
 }
 window.addEventListener('load', init)
+
+
+/* function Log in Google
+function registrar(){
+    console.log("Click!! funciona")
+}
+
+//button log in Google
+const loginGoogle = document.getElementById('btnGmail');
+if (loginGoogle){
+loginGoogle.addEventListener('click', registrar);
+}*/
