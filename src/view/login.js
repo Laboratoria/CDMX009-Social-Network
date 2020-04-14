676a817e9512ee5114140559101efcd2194f7c81
Fernanda de la Peña ☆ <kapirupia@gmail.com>
2020-04-14 10:08:30 -0500

    you can now login w/firebase auth :D

---
+++ src/view/login.js
@@ -0,0 +1,36 @@
+import { changeView } from '../view-controler/router.js'
+
+export default () => {
+  const viewLogin = `
+    <form>
+    <input type='email' placeholder='Email' id="emailText"> </input>
+    <input type='password' placeholder='Password' id="passwordText"> </input>
+    <button class='btn' id='loginBtn'> puchame </button>
+    </form>
+  `;
+
+  //nodes (for the creation of the HTML elements)
+  const divElement = document.createElement('div')
+  divElement.innerHTML = viewLogin
+
+  //nodes (to get the DOM elements inside the form and initialize the login function)
+  const emailText = divElement.querySelector('#emailText')
+  const passwordText = divElement.querySelector('#passwordText')
+  const loginBtn = divElement.querySelector('#loginBtn')
+
+  //login event
+  loginBtn.addEventListener('click', e => {
+    const email = emailText.value
+    const pass = passwordText.value 
+    const auth = firebase.auth()
+
+  //sign in with firebase functions
+  const promise = auth.signInWithEmailAndPassword(email, pass)
+  promise.then (e=> changeView('#/home'))
+  promise.catch(e=>console.log(alert(e.message)))
+
+  console.log('jiji')
+  })
+
+  return divElement
+}
