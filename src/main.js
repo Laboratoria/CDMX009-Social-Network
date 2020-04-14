da91bea8f802dc11f6d3c203e9016f632b9ebccf
Fernanda de la Peña ☆ <kapirupia@gmail.com>
2020-04-13 21:26:19 -0500

    Login w/ email + password
    You can login with an existing account, if the user exists it redirects you to the feed, if it doesn't it sends an alert message
    
    (test account  -- email: test@testing.com password: 123456).

--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
-import { changeView } from './view-controler/index.js'
+import { changeView } from './view-controler/router.js'
+
 
 const init = () => {
   changeView(window.location.hash)
   window.addEventListener('hashchange', () => changeView(window.location.hash))
 }
 
+const initFirebase = () => {
+   // Your web app's Firebase configuration
+   var firebaseConfig = {
+    apiKey: "AIzaSyA9A0nhwI6q1W6-72LbE4LC9Mi9i3nWSew",
+    authDomain: "bichigra-m.firebaseapp.com",
+    databaseURL: "https://bichigra-m.firebaseio.com",
+    projectId: "bichigra-m",
+    storageBucket: "bichigra-m.appspot.com",
+    messagingSenderId: "349399643965",
+    appId: "1:349399643965:web:f1b27146da69266891b754",
+    measurementId: "G-VSB54NJVLP"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+}
+
+window.addEventListener('load', initFirebase)
 window.addEventListener('load', init)
