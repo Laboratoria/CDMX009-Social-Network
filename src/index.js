627ac604b6f6442f869079e63d44aa5dc93e0d5c
Sheiki1 <kirokomerteabesos@gmail.com>
2020-04-16 12:15:46 -0500

    se agregaron estilos con bulma y ya se puede cambiar de registro a iniciar sesion sin problema

--- src/index.js
+++ src/index.js
@@ -8,60 +8,151 @@ function paginaDeInicio(){
     root.innerHTML = " "
     let homeView = document.createElement('div')
     homeView.innerHTML = `
+    <article class="media">
+        <div class="media-center">
         <div id="mainImages" >
-        <figure class="image is-2by1">
-  <img src="imagen1.png">
-</figure>
-<figure class="image">
+            <figure class="image is-2by1">
+                <img src="imagen1.png">
+            </figure>
+            <div class="columns is-mobile is-centered">
+                <figure class="image">
+                    <img class="is-rounded" id="logo"  src="logo.png " >
+                </figure>
+            </div>
+        </div> 
+        <br>
+        <div class="columns is-mobile is-centered">
+            <h1 class="title has-text-centered">¡Bienvenidx repostero!</h1>
+        </div>
+        <div class="field">
+            <p class="control has-icons-left has-icons-right">
+                <input class="input is-rounded" type="email" placeholder="Email" id="si">
+                <span class="icon is-small is-left">
+                    <i class="fas fa-envelope"></i>
+                </span>
+                <span class="icon is-small is-right">
+                    <i class="fas fa-check"></i>
+                </span>
+            </p>
+            </div>
+            <div class="field">
+            <p class="control has-icons-left">
+                <input class="input is-rounded" type="password" placeholder="Password">
+                <span class="icon is-small is-left">
+                    <i class="fas fa-lock"></i>
+                </span>
+            </p>
+            </div>
+            <div class="field is-grouped is-grouped-centered">
+            <button class="button is-rounded "  id="logIn">LOG IN</button>
+            </div>
+        <div class="columns is-mobile is-centered">
+            <p class="subtitle">Ingresa con</p>
+        </div>
+        <a class="level-item">
+        <img src="faceImage.png" class="face">
+        <div style="width: 100px; height: 39px" ></div>
+        <img src="gmailImage.png" class="gmail">
+        </a>
+        <div id="fin">
+            <p class="subtitle has-text-centered">No tienes cuenta? <button id="liga" class="subtitle">Regístrate</button> gratis</p>
+        <br>
+        </div>
+        </div>
+        </article>
 
-  <img class="is-rounded" style=" width:133px; height:77px" src="logo.png " frameborder="0" allowfullscreen>
-</figure>
-</div> 
-<h1 class="title">¡Bienvenidx repostero!</h1>
-<input class="input is-rounded" value="E-mail"></input>
-<input class="input is-rounded" value="password" type="password"></input>
-<button class="button is-rounded"  id="logIn">Log In</button>
-<p class="subtitle">Ingresa con</p>
-<img src="https://pixabay.com/get/51e7d545425ab108feda8460c62d3f7d1737d8e34e507441732d73d2974ecc_1920.png" class="face">
-<img src="https://pixabay.com/get/57e1d3414352ad14f6d1867dda293276163dd6e3554c704c7d2979dc914cc35e_1920.png" class="gmail">
-  <p class="subtitle">No tienes cuenta? <button id="liga" class="button">Regístrate</button> gratis</p>
-<br>
     `
-    //image is-5by3
   root.appendChild(homeView)  
+  let loginButton = document.querySelector('#liga')
+  loginButton.onclick = renderLogin
 }
 
-paginaDeInicio()
+//paginaDeInicio()
 
-let loginButton = document.querySelector('#liga')
+//let loginButton = document.querySelector('#liga')
 function renderLogin(){
     //root.innerHTML =" "
     let homeView = `
-        <div id="mainImages" >
-  <img src="https://pixabay.com/get/54e7d14a4852a814f6d1867dda293276163dd6e3554c704c7d2979dc9448c75a_1920.jpg" class="firstImage">
-  <img src="https://pixabay.com/get/55e8d544495ba514f6da8c7dda293276163dd6e3554c704c7d2979dc924dc050_1280.png" class="secondImage">
-</div> 
-<h1 class="title">¡Registrate!</h1>
-<input class="input" value="   E-mail"></input>
-<input class="input" value="   User name"></input>
-<input type="password" class="input" value="   Password"></input>
-<input type="password" class="input" value="   Password confirm"></input>
-<button class="button">Log In</button>
-<p class="subtitle">Ingresa con</p>
-<img src="https://pixabay.com/get/51e7d545425ab108feda8460c62d3f7d1737d8e34e507441732d73d2974ecc_1920.png" class="face2">
-<img src="https://pixabay.com/get/57e1d3414352ad14f6d1867dda293276163dd6e3554c704c7d2979dc914cc35e_1920.png" class="gmail2">
-  <p class="subtitle">Ya tienes cuenta? <button id="liga2" class="liga2">inicia sesion </button></p>
-<br>
-    `
-  root.innerHTML = homeView
-  
- let singUpButton = document.querySelector('#liga2')
-//singUpButton.onclick = root.innerHTML = " "
-singUpButton.onclick = paginaDeInicio 
- loginButton.onclick = renderLogin
-}
-loginButton.onclick = renderLogin
+    <article class="media">
+        <div class="media-center">
+            <div id="mainImages" >
+                <figure class="image is-2by1">
+                    <img src="imagen1.png">
+                </figure>
+                <div class="columns is-mobile is-centered">
+                    <figure class="image">
+                        <img class="is-rounded" id="logo"  src="logo.png " >
+                    </figure>
+                </div>
+            </div> 
+            <br>
+            <div class="columns is-mobile is-centered">
+                <h1 class="title has-text-centered">¡Registrate!</h1>
+            </div>
+            <div class="field">
+                <p class="control has-icons-left has-icons-right">
+                    <input class="input is-rounded" type="email" placeholder="Email">
+                    <span class="icon is-small is-left">
+                        <i class="fas fa-envelope"></i>
+                    </span>
+                    <span class="icon is-small is-right">
+                        <i class="fas fa-check"></i>
+                    </span>
+                </p>
+            </div>
+            <div class="field">
+                <p class="control has-icons-left has-icons-right">
+                    <input class="input is-rounded" type="text" placeholder="User name">
+                    <span class="icon is-small is-left">
+                        <i class="fas fa-user"></i>
+                    </span>
+                    <span class="icon is-small is-right">
+                        <i class="fas fa-check"></i>
+                    </span>
+                </p>
+            </div>
+            <div class="field">
+                <p class="control has-icons-left">
+                    <input class="input is-rounded" type="password" placeholder="Password">
+                    <span class="icon is-small is-left">
+                        <i class="fas fa-lock"></i>
+                    </span>
+                </p>
+            </div>
+            <div class="field">
+                <p class="control has-icons-left">
+                    <input class="input is-rounded" type="password" placeholder="Password confirm">
+                    <span class="icon is-small is-left">
+                        <i class="fas fa-lock"></i>
+                    </span>
+                </p>
+            </div>
+            <div class="field is-grouped is-grouped-centered">
+                <button class="button is-rounded"  id="singUp">SING UP</button>
+            </div>
+            
+            <p class="subtitle has-text-centered">Ingresa con</p>
+            <a class="level-item">
+                <img src="faceImage.png" class="face2" style="width: 43px; height: 39px">
+                <div style="width: 100px; height: 39px" ></div>
+                    <img src="gmailImage.png" class="gmail2" style="width: 43px; height: 39px">
+             </a>
+            <div id="fin">
+                <p class="subtitle has-text-centered">Ya tienes cuenta? <button id="liga2" class="subtitle">Inicia sesion </button></p>
+            </div>
+            <br>
+        </div>
+    </article>
 
+    `
+    root.innerHTML = homeView
+    let singUpButton = document.querySelector('#liga2')
+    //singUpButton.onclick = root.innerHTML = " "
+    singUpButton.onclick = paginaDeInicio 
+   // loginButton.onclick = renderLogin
+}
+//loginButton.onclick = renderLogin
+paginaDeInicio()
 // let singUpButton = document.querySelector('#liga2')
 //singUpButton.onclick = root.innerHTML = " "
 //singUpButton.onclick = paginaDeInicio
