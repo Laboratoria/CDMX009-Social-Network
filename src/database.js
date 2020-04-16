4b9b01eec9b40dc5fb6c317ec206471fa2c140aa
Nallely <nallely232@hotmail.com>
2020-04-15 21:11:35 -0500

    Se termino la conexion con FB, se añadio el observador del usuario y se comenzo la segunda pantalla de registro con imagen y nombre de usuario

--- src/database.js
+++ src/database.js
@@ -1,27 +1,100 @@
-const database = {
-  signUp: () => {
-    
-    // console.log ('diste un clic')
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-  
-  console.log (email);
-  console.log (password);
-  firebase.auth().createUserWithEmailAndPassword(email, password)
-  .catch(function(error) {
-// Handle Errors here.
-var errorCode = error.code;
-var errorMessage = error.message;
-if (errorCode == 'auth/weak-password') {
-  alert('The password is too weak.');
-} else {
-  alert(errorMessage);
-}
-console.log(error);
-});
-  
-  },
+import { renderFirstProfile } from './index.js';
 
+const provider = new firebase.auth.GoogleAuthProvider();
+const provider1 = new firebase.auth.FacebookAuthProvider();
+
+const database = {
+  signUp: () => { 
+    const regEmail = document.getElementById('regEmail').value;
+    const regPassword = document.getElementById('regPassword').value;
+    firebase.auth().createUserWithEmailAndPassword(regEmail, regPassword)
+      .catch(function(error) {
+        let errorCode = error.code;
+        let errorMessage = error.message;
+        if (errorCode == 'auth/weak-password') {
+          alert('The password is too weak.');
+        } else {
+          alert(errorMessage);
+        }
+        console.log(error);
+        });
+  },
+  signIn: () => {
+    let logEmail = document.getElementById('logEmail').value;
+    let logPassword = document.getElementById('logPassword').value;
+    firebase.auth().signInWithEmailAndPassword(logEmail, logPassword)
+      .catch(function(error) {
+        let errorCode = error.code;
+        let errorMessage = error.message;
+        console.log(errorCode);
+        console.log(errorMessage);
+      });
+  },  
+  
+  userObserver: () => {
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        console.log('existe usuario activo');
+        renderFirstProfile();
+        console.log('*****************');
+        console.log(user.emailVerified);
+        console.log('*****************');
+        // userShow();
+        // User is signed in.
+        var displayName = user.displayName;
+        var email = user.email;
+        var emailVerified = user.emailVerified;
+        var photoURL = user.photoURL;
+        var isAnonymous = user.isAnonymous;
+        var uid = user.uid;
+        var providerData = user.providerData;
+        // ...
+      } else {
+        console.log('no existe usuario activo');
+        // User is signed out.
+        // ...
+      }
+    }); 
+  },
+  
+  signInGoogle: () => {
+    firebase.auth().signInWithPopup(provider)
+      .then(function(result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        let token = result.credential.accessToken;
+        // The signed-in user info.
+        let user = result.user;
+        // ...
+      }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
+  },
+  signInFacebook: () => {
+    firebase.auth().signInWithPopup(provider1).then(function(result) {
+      // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      // ...
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
+  }
 };
 
+database.userObserver();
 export default database;
--- src/index.js
+++ src/index.js
@@ -1,40 +1,200 @@
 import database from './database.js';
 
 const root = document.querySelector('#root');
-const logo = `<img width="200px" class="mainLogo" src="https://i.ibb.co/sFBwWCc/memingos-rgb.png">`
+const logo = `<div class="hero-body "><img width="200px" class="mainLogo" src="https://i.ibb.co/sFBwWCc/memingos-rgb.png"></div>`;
+const logoM = `<div class="hero-body "><img width="200px" class="logoMemingos" src="https://i.ibb.co/yhwCvy1/logo-m-new-rgb.png"></div>`;
 
-const renderLogin = () => {
-    const loginForm = `
-    <div class="field">
-        <div class= "file is-small file is-centered"> 
-            <p class="control">
-              <input id="email" class="input" type="email" placeholder="Email">
+const renderSignIn = () => {
+    const signInForm = `
+    <div class="has-text-centered has-text-white title is-4">
+        <h4> Bienvenido(a) </h4>
+    </div>
+        <div class="field">
+            <div class= "file is-small file is-centered"> 
+                <p class="control has-icons-left has-icons-right">
+                    <input id="logEmail" class="input is-rounded " type="email" placeholder="Email">
+                    <span class="icon is-small is-left">
+                    <i class="fas fa-envelope"></i>
+                </span>
+                <span class="icon is-small is-right">
+                    <i class="fas fa-check"></i>
+                </span>
+                </p>
+            </div>
+        </div>
+        <div class="field file is-small file is-centered">
+            <p class="control has-icons-left has-icons-right">
+                <input id="logPassword" class="input is-rounded " type="password" placeholder="Password">
+                <span class="icon is-small is-left">
+                    <i class="fas fa-lock"></i>
+                </span>
+            </p>
+        </div> 
+        <div class="field">
+            <div class="file is-centered">
+                <p class="control">
+                    <button id="logIn" class="button is-success button is-medium  has-background-warning is-rounded">
+                    INGRESAR
+                    </button>   
+                </p>
+            </div>
+        </div>
+        <div class="has-text-centered has-text-white title is-6">
+        <h6>O ingresa con.. </h6>
+    </div>
+        <div class="buttons file is-centered ">
+            <a id="facebookSignIn" class="button is-medium is-facebook">
+               <span class="icon">
+                 <i class="fab fa-facebook"> </i>
+              </span>
+            </a>
+            <a id="googleSignIn" class="button is-medium is-google">
+                 <span class="icon">
+                    <i class="fab fa-google"></i>
+                </span>
+            </a>
+        </div>
+        
+            <div class=" has-text-centered has-text-white is size-2">
+                <td> ¿No tienes cuenta? </td> <a id="singUpLink"> Regístrate </a>
+            </div>`;
+        root.innerHTML = `${logo}${signInForm}`;
+};
+document.addEventListener('click',function(event) {
+    if (event.target && event.target.id === 'logIn') {
+        database.signIn();
+    }
+});
+const renderSignUp = () => {
+    const signUpForm = `
+     
+        <div class="has-text-centered has-text-white title is-4 ">
+            <h4> Bienvenido(a) </h4>
+        </div>
+ 
+        <div class="field file is-small file is-centered ">
+            <p class="control has-icons-left has-icons-right ">
+                <input id="regEmail" class="input is-rounded " type="email" placeholder="Email">
+                <span class="icon is-small is-left ">
+                    <i class="fas fa-envelope"></i>
+                </span>
+                <span class="icon is-small is-right">
+                    <i class="fas fa-check"></i>
+                </span>
             </p>
         </div>
-    </div>
-    <div class="field">
-        <div class="file is-small file is-centered">
-        <p class="control">
-            <input id="password" class="input" type="password" placeholder="Password">
-        </p>
+        <div class="field file is-small file is-centered">
+            <p class="control has-icons-left has-icons-right">
+                <input id="regPassword" class="input is-rounded" type="password" placeholder="Password">
+                <span class="icon is-small is-left">
+                    <i class="fas fa-lock"></i>
+                </span>
+            </p>
+        </div>  
+        <div class="field">
+            <div class="file is-centered">
+                <p class="control ">
+                    <button  id="register" class=" button is-success button is-medium  has-background-warning is-rounded">
+                    REGISTRAR
+                    </button>   
+                </p>
+            </div>
         </div>
+    <div class="has-text-centered has-text-white title is-6">
+        <h6>O ingresa con.. </h6>
+    </div>
+        <div class="buttons file is-centered ">
+            <a id="facebookSignIn" class="button is-medium is-facebook">
+               <span class="icon">
+                 <i class="fab fa-facebook"> </i>
+              </span>
+            </a>
+            <a id="googleSignIn" class="button is-medium is-google">
+                 <span class="icon">
+                    <i class="fab fa-google"></i>
+                </span>
+            </a>
+        </div>
+    
+    
+        <div class=" has-text-centered has-text-white">
+            <td> ¿Ya tienes cuenta? </td> <a id="singInLink"> Inicia sesión </a>
+        </div>
+         `;
+
+    root.innerHTML = `${logo}${signUpForm}`;
+};
+
+document.addEventListener('click',function(event) {
+    if (event.target && event.target.id === 'register') {
+        database.signUp();
+    } 
+ });
+
+ document.addEventListener('click',function(event) {
+    const item = event.target.closest('a');
+   if (item && item.id === 'facebookSignIn') {
+       database.signInFacebook();
+   } 
+});
+
+document.addEventListener('click',function(event) {
+    const item = event.target.closest('a');
+   if (item && item.id === 'googleSignIn') {
+       database.signInGoogle();
+   } 
+});
+document.addEventListener('click',function(event) {
+    if (event.target && event.target.id === 'singInLink') {
+        renderSignIn();
+    }
+});
+document.addEventListener('click',function(event) {
+    if (event.target && event.target.id === 'singUpLink') {
+        renderSignUp();
+    }
+});
+renderSignUp();
+
+
+export const renderFirstProfile = () => {
+    root.classList.remove("signUpAndIn");
+    const firstProfile = `
+    <figure class="image is-96x96">
+        <img src="https://bulma.io/images/placeholders/128x128.png">
+    </figure>
+
+        <div class="file is-centered">
+            <label class="file-label">
+                <input class="file-input" type="file" name="profile">
+                <span class="file-cta">
+                  <span class="file-icon">
+                    <i class=fas fa-upload"> </i>
+                  </span>
+                  <span class="file-label">
+                  Añadir foto de perfil
+                </span>
+              </span>
+            </label>
+          </div>
+        <div class="field">
+          <div class="control">
+            <input class="input is-primary" type="text">
+          </div>
+        </div>
+    <div class="has-text-centered has-text-black title is-6">
+        <h6> Username </h6>
     </div>
     <div class="field">
         <div class="file is-centered">
             <p class="control">
-                <button id="register" class="button is-success button is-medium  has-background-warning">
-                Registrarse
+                <button  id="confirm" class=" button is-success button is-medium  has-background-warning is-rounded">
+                CONFIRMAR
                 </button>   
             </p>
-       </div>
-    </div>`;
-    root.innerHTML = `${logo}${loginForm}`;
+        </div>
+    </div>
+    `
+    root.innerHTML = `${logoM}${firstProfile}`;
 };
-const register = document.querySelector('#register');
-document.addEventListener('click',function(event){
-    if(event.target && event.target.id== 'register'){
-        database.signUp();
-    }
-});
 
-renderLogin();
