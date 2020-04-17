233e0bb095d718b4e3efd0aeefdba8aae2452425
vanyaxel <vanya_xel@live.com.mx>
2020-04-16 23:26:00 -0500

    archivos para SPA

---
+++ src/view-controller/router.js
@@ -0,0 +1,28 @@
+import { components } from '../view/index.js';
+
+export const changeTmp = (hash) => {
+    const id = hash.split('/')[1];
+    const sectionMain = document.getElementById('contentCreated');
+    sectionMain.innerHTML = '';
+
+    switch (hash) {
+        case '':
+        case '#':
+        case '#/':
+        case '#/home':
+            { return sectionMain.appendChild(components.home()); }
+        case '#/profile':
+        case '#/favorites':
+        case '#/reviews':
+        case '#/saved':
+        case '#/conexions':
+        case '#/followers':
+        case '#/follow':
+        case '#/menu':
+        case '#/newPost':
+
+            { return sectionMain.appendChild(components[id]()); }
+        default:
+            return sectionMain.appendChild(components.different())
+    }
+};
