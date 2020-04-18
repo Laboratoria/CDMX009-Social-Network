181db65a8819da68b02590efcc942ea8aa4e289c
Oralia <oraliagarrido@gmail.com>
2020-04-18 18:15:35 -0500

    El usuario puede ingresar con su cuenta de google

--- src/index.js
+++ src/index.js
@@ -4,8 +4,18 @@ const logInButton = document.querySelector('#logIn');
 
 const creatAccountButton = document.querySelector('#creatAccount');
 
+const logInGoogleButton = document.querySelector('#logInGoogle');
+
+const logInFacebookButton = document.querySelector('#logInFacebook');
+
+var provider = new firebase.auth.GoogleAuthProvider(); //proveedor del servicio
+
 function createAccount() {
   
+  document.getElementById('logInUser').style.display="none";
+  document.getElementById('init').style.display="none";
+  document.getElementById('logInNetwoork').style.display="none";
+  document.getElementById('LogInNewUser').style.display="none";
    let createAccountView = `
       <div class="login">
         
@@ -30,6 +40,9 @@ function createAccount() {
 
    
 function logIn(){
+
+  document.getElementById('logInNetwoork').style.display="none";
+  document.getElementById('LogInNewUser').style.display="none";
     
   let emailLogIn = document.querySelector('#emailLogIn').value
   let passLogIn = document.querySelector('#passLogIn').value
@@ -57,3 +70,52 @@ function logIn(){
   }
 
   logInButton.onclick = logIn;
+
+  function logInGoogle (){
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      // ...
+      console.log(user);
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
+    
+  }
+  
+
+  logInGoogleButton.onclick = logInGoogle;
+
+function logInFacebook () {
+
+  const provider = new firebase.auth.FacebookAuthProvider();
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+    /// This gives you a Facebook Access Token. You can use it to access the Facebook API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+   var user = result.user;
+    // ...
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+   var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+   var credential = error.credential;
+    // ...
+  }
+  );
+
+}
+
+  logInFacebookButton.onclick = logInFacebook;
