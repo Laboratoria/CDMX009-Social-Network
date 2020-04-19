729dead380daa99d294854f1a99e1fe0587d8e00
EdithOrt <eoc.diseno@gmail.com>
2020-04-19 17:16:22 -0500

    Screen to upload images to the store with functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-import { example } from './example.js';
+import singIn from './login.js'
 
-example();
+function init (){
+    singIn()
+}
+
+
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBuxP-xlUpYjWc5KNNt6q0hKbsxc8L4cE4",
+    authDomain: "cdmx009-f4cac.firebaseapp.com",
+    databaseURL: "https://cdmx009-f4cac.firebaseio.com",
+    projectId: "cdmx009-f4cac",
+    storageBucket: "cdmx009-f4cac.appspot.com",
+    messagingSenderId: "206411662308",
+    appId: "1:206411662308:web:8c3266d5640e307ca57df1",
+    measurementId: "G-L6NB5984DB"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+  firebase.firestore();
+  window.addEventListener('load', firebaseConfig);
+  window.addEventListener('load', init);
