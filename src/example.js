de031f9a8857a1a3314e1667d6dca0e92ce6a4a7
LizethRivera04 <60666768+LizethRivera04@users.noreply.github.com>
2020-04-20 15:15:40 -0500

    Login settings and popup functionality

--- src/example.js
+++ src/example.js
@@ -1,153 +1,122 @@
-/* export const example = () => {
-    // aquí tu código
-} */
+import vista from '/vista.js'
+import login from '/login.js'
+import signIn from '/signIn.js'
+import { loginGoogle } from '/index.js'//Se importa para usar addEventListener
+import { loginFacebook } from '/index.js'
+import { createUser } from '/index.js'
+import { loginUser } from '/index.js'
+import { signoutUser } from '/index.js'
 
-// Are you sure that you want to delete the element?
-const overDelete = document.createElement('div');
-overDelete.className = 'overSmall';
-overDelete.id = 'overDelete';
-overDelete.innerHTML = `
-    <div class = "popSmall">
-        <div class = "containerPop">
-            <div class = "closeButton">
-                <a href = "#" id="btn-cerrar-delete" class = "btn-cerrar-popup"><i class = "far fa-times-circle"></i></a>
-            </div>
-            <div class = "textSmall">
-                <a href = "#" id="youSure" class = "imagePop"><i class="far fa-frown-open"></i></a>
-                <p>Estás seguro que quieres eliminar el elemento?</p>
-                
-                <a href = "#" id="acceptDelete" class = "acceptDelete"><i class="fas fa-check"></i></a>
-                <a href = "#" id="cancelDelete" class = "cancelDelete"><i class="fas fa-times"></i></a>
-            </div>
-            
-        </div>
-    </div>
-`
-document.body.appendChild(overDelete);
+//Boton para mostrar iniciar sesión
+const btnshow = document.querySelector('#showLogin')
+btnshow.addEventListener('click', principalView)
 
+//Boton para cerrar sesión
+const btnExit = document.querySelector('#goOut')
+btnExit.addEventListener('click', signoutUser)
 
-
-// Open PopUpDelete
-const btnPop = document.querySelector('#pop')
-const overlayDelete = document.querySelector('#overDelete');// el id lo tiene el overlay
-const popup = document.querySelector('.popSmall');
-const btnClosePopup = document.querySelector('#btn-cerrar-delete');
-btnPop.addEventListener('click', abrirPopup);
-btnClosePopup.addEventListener('click', cerrarPopup);
-
-function abrirPopup() {
-    overlayDelete.classList.add('active');
-    popup.classList.add('active');
-}
-function cerrarPopup() {
-    overlayDelete.classList.remove('active');
-    popup.classList.remove('active');
+//Mostrar vista (una antes de iniciar sesión)
+function principalView() {
+    const divRoot = document.querySelector('#root')
+    divRoot.innerHTML = vista()
+    let goLogin = document.querySelector('#goLogin')
+    goLogin.addEventListener('click', viewLogin)
 }
 
+//Mostrar vista iniciar sesión
+export function viewLogin() {
+    const divRoot = document.querySelector('#root')
+    divRoot.innerHTML = login()
+    let goSignIn = document.querySelector('#goSignIn')
 
-//Task Completed
+    //Listeners modales
+    const btnCloseError = document.querySelector('#btn-cerrar-error');
+    btnCloseError.addEventListener('click', closeError);
+    const errorGooFbkModal = document.querySelector('#noAccountGooFbk');
+    const btnCloseGoogleError = document.querySelector('#btn-cerrar-noGooFbk');
+    btnCloseGoogleError.addEventListener('click', closeGooFbkError)
 
-const overComplete = document.createElement('div');
-overComplete.className = 'overSmall';
-overComplete.id = 'overComplete';
-overComplete.innerHTML = `
-    <div class = "popSmall">
-        <div class = "containerPop">
-            <div class = "closeButton">
-                <a href = "#" id="btn-cerrar-complete" class = "btn-cerrar-popup"><i class = "far fa-times-circle"></i></a>
-            </div>
-            <div class = "textSmall">
-                <a href = "#" id="dataComplete" class = "imagePop"><i class="fas fa-crow"></i></a>
-                <p>Se han guardado tus cambios </p>
-            </div>
-
-        </div>
-    </div>
-`
-document.body.appendChild(overComplete);
-
-
-// Open PopUp Task Complete
-const btnComplete = document.querySelector('#taskCompleted')
-const overComp = document.querySelector('#overComplete');// el id lo tiene el overlay
-const popuptask = document.querySelector('.popSmall');
-const btnCloseTask = document.querySelector('#btn-cerrar-complete');
-btnComplete.addEventListener('click', openComplete);
-btnCloseTask.addEventListener('click', closeComplete);
-
-function openComplete() {
-    overComp.classList.add('active');
-    popuptask.classList.add('active');
-}
-function closeComplete() {
-    overComp.classList.remove('active');
-    popuptask.classList.remove('active');
+    //Ingresar
+    let btnEnter = document.querySelector('#enter')
+    btnEnter.addEventListener('click', function (e) {
+        e.preventDefault()
+        let email = document.querySelector('#email').value
+        let password = document.querySelector('#password').value
+        const errorModal = document.querySelector('#errorModal');
+        /* console.log(email);
+        console.log(password); */
+        loginUser(email, password, errorModal)
+    })
+    //Ocultar Modaless
+    function closeError() {
+        errorModal.classList.remove('is-active');
+    }
+    function closeGooFbkError() {
+        errorGooFbkModal.classList.remove('is-active');
+    }
+    viewSign(goSignIn)
+    loginGoogle(errorGooFbkModal)
+    loginFacebook(errorGooFbkModal)
 }
 
+//Mostrar vista Resgistro
+export function viewSign(goSignIn) {
+    goSignIn.addEventListener('click', (e) => {
+        e.preventDefault()
+        const divRoot = document.querySelector('#root')
+        divRoot.innerHTML = signIn()
 
-//Show Error
+        //Listeners Modales
+        const btnCloseError = document.querySelector('#btn-cerrar-error');
+        btnCloseError.addEventListener('click', closeError);
+        const btnCloseExist = document.querySelector('#btn-cerrar-exist-error');
+        btnCloseExist.addEventListener('click', closeErrorExist);
+        let btnBack = document.querySelector('#backOne')
+        const errorGooFbkModal = document.querySelector('#noAccountGooFbk');
+        const btnCloseGoogleError = document.querySelector('#btn-cerrar-noGooFbk');
+        btnCloseGoogleError.addEventListener('click', closeGooFbkError)
 
-const overError = document.createElement('div');
-overError.className = 'overSmall';
-overError.id = 'overError';
-overError.innerHTML = `
-    <div class = "popSmall">
-        <div class = "containerPop">   
-            <div class = "textSmall">
-                <h1>ERROR</h1>
-                <a href = "#" id="error" class = "imagePop"><i class="far fa-dizzy"></i></a>
-                <p>Intenta de nuevo, alguno de tus datos no coincide</p>
-            </div>
-            <div class = "closeButton">
-                <a href = "#" id="btn-cerrar-error" class = "btn-cerrar-popup"><i class="fas fa-check"></i></a>
-            </div>
+        //Crear cuenta
+        let btnRegister = document.querySelector('#signIn')
+        btnRegister.addEventListener('click', function (e) {
+            e.preventDefault()
+            //Leer variables del form
+            const newName = document.querySelector('#newName').value
+            const newEmail = document.querySelector('#newEmail').value
+            const newPassword = document.querySelector('#newPassword').value
+            const registryModal = document.querySelector('#registryModal');// el id lo tiene el overlay
+            const alreadyExistModal = document.querySelector('#alreadyExistModal');
+            /* console.log(newName);
+            console.log(newEmail);
+            console.log(newPassword); */
+            createUser(newName, newEmail, newPassword, registryModal, alreadyExistModal)
 
-        </div>
-    </div>
-`
-document.body.appendChild(overError);
+        })
+        //Ocultar Modales
+        function closeError() {
+            registryModal.classList.remove('is-active');
+        }
+        function closeErrorExist() {
+            alreadyExistModal.classList.remove('is-active');
+        }
+        function closeGooFbkError() {
+            errorGooFbkModal.classList.remove('is-active');
+        }
+        arrowBack(btnBack)
+        loginGoogle(errorGooFbkModal)
+        loginFacebook(errorGooFbkModal)
+    })
 
-
-// Open PopUp Task Complete
-const btnError = document.querySelector('#showError')
-const overErr = document.querySelector('#overError');// el id lo tiene el overlay
-const popupError = document.querySelector('.popSmall');
-const btnCloseError = document.querySelector('#btn-cerrar-error');
-btnError.addEventListener('click', openError);
-btnCloseError.addEventListener('click', closeError);
-
-function openError() {
-    overErr.classList.add('active');
-    popupError.classList.add('active');
 }
-function closeError() {
-    overErr.classList.remove('active');
-    popupError.classList.remove('active');
+
+//Regresar a inicio de sesión
+function arrowBack(btnBack) {
+    btnBack.addEventListener('click', (e) => {
+        e.preventDefault()
+        const divRoot = document.querySelector('#root')
+        divRoot.innerHTML = login()
+        viewLogin()
+    })
 }
 
 
-
-/* const profileCard = document.createElement('div');
-profileCard.className = 'profileCard';
-profileCard.innerHTML = `
-    <div class = "containerPop">
-    <div class = "closeButton">
-    <a href = "#" id="btn-cerrar-popup" class = "btn-cerrar-popup"><i class = "far fa-times-circle"></i></a></div>
-    <p>Estás seguro que quieres eliminar el elemento?</p>
-
-    </div>
-
-
-`
-profileContainer.appendChild(profileCard); */
-
-/* const closeButton = document.createElement('div');
-closeButton.className = 'closeButton';
-profileCard.appendChild(closeButton); */
-
-/* const close = document.createElement('a');
-close.className = 'btn-cerrar-popup';
-closeButton.appendChild(close);
-const icon = document.createElement('i');
-icon.className = 'far fa-times-circle';
-close.appendChild(icon); */
---
+++ src/vista.js
@@ -0,0 +1,7 @@
+export default () => {
+    const vista = `
+        <button class = "button is-rounded container-separate is-primary" id="goLogin">Iniciar</button>
+    
+    `
+    return vista
+}
