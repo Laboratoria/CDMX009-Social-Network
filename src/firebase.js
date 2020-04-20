59db0907d500cdde2721c161cf19d7f18cafe0d2
IrisFyD <iriis.trejo.c@gmail.com>
2020-04-20 16:49:34 -0500

    add function saveDataUser

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,16 @@
+//Ref firebase
+let providerG = new firebase.auth.GoogleAuthProvider();
+let providerFB = new firebase.auth.FacebookAuthProvider();
+let db = firebase.firestore();
+
 
 //Login Google
 function loginGoogle(){
-  let providerG = new firebase.auth.GoogleAuthProvider();
   firebase.auth()
   .signInWithPopup(providerG)
   .then(function(result) {
-    console.log(result.user);
+    //console.log(result.user);
+    saveDataUser(result.user);
     if (result.user.emailVerified){
       window.open('#home','_self')
     }
@@ -13,11 +18,27 @@ function loginGoogle(){
 }
 
 function loginFB(){
-  let providerFB = new firebase.auth.FacebookAuthProvider();
   firebase.auth()
   .signInWithPopup(providerFB)
   .then(function(result) {
-    console.log(result.user);
+    //console.log(result.user);
+    saveDataUser(result.user);
+    if (result.user.emailVerified){
+      window.open('#home','_self')
+    }
   });
 }
+//Save user by login
+function saveDataUser(user){
+let userNew = {
+  uid:user.uid,
+  name:user.displayName,
+  photo:user.photoURL
+}
+db.collection("users").doc(userNew.uid).set(userNew)
+.then(function() {
+  console.log("Document successfully written!");
+})
+};
+
 export  { loginGoogle, loginFB };
