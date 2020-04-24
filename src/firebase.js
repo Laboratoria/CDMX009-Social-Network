e3cdd624a225ea896abf5972cc26087befeb94f7
IrisFyD <iriis.trejo.c@gmail.com>
2020-04-24 15:20:13 -0500

    function showPostUser errorg fixed

--- src/firebase.js
+++ src/firebase.js
@@ -29,8 +29,7 @@ function loginFB(){
 }
 
 function createEmailPass(email, password, names) {
-  firebase
-    .auth()
+  firebase.auth()
     .createUserWithEmailAndPassword(email, password)
     .then((result) => {
       saveDataUser(result.user);
@@ -70,17 +69,17 @@ function saveDataUser(user){
 
 //add post user
 function addUserPost(){
-  let post = document.getElementById("post").value;
-  let postNewPrint = document.querySelector("#postNew");
-  console.log(postNewPrint.appendChild("<p>prueba</p>"));
+  let user = firebase.auth().currentUser;
+  let post = document.getElementById("addNewPost").value;
   db.collection("post").add({
     post: post,
+    user: user.uid
 })
 .then(function(docRef) {
   console.log("Document written with ID: ", docRef.id);
-    document.getElementById("post").value = '';
-    
+    document.getElementById("addNewPost").value = '';
     showPostUser();
+    
 })
 .catch(function(error) {
     console.error("Error adding document: ", error);
@@ -89,15 +88,27 @@ function addUserPost(){
 
 //Show post user
 function showPostUser(){
-  db.collection("post").onSnapshot((querySnapshot) => {
+  const postContainer = document.getElementById('allNewPost');
+        postContainer.innerHTML='';
+        db.collection("post").onSnapshot((querySnapshot) => {
         querySnapshot.forEach((doc) => {;
         console.log(`${doc.id} => ${doc.data().post}`);
-        /*let postNew = `
-        <p>${doc.id}</p>
-        `
-        postNewPrint.appendChild(postNew)*/
+        let postElement = document.createElement('div');
+        let postNew = `
+        <div class="conteinerPostPrint">
+          <img class="imgUser" src="img/user.svg"></img> <p class="nameUser">Srta</p>
+          <p class="PostPrint">${doc.data().post}</p>
+          <textarea class="answer" id="answer"> </textarea>
+          <p class="nroLikes">0</p>
+          <img class="imgLikes" src="img/like.svg"></img>
+          <button class="btnAnswer">Responder</button>
+        </div>
+        `;
+        postElement.innerHTML = postNew;
+
+        postContainer.appendChild(postElement);
       });
 });
 }
 
-export  { loginGoogle, loginFB, registerUser, addUserPost };
+export  { loginGoogle, loginFB, registerUser, addUserPost, showPostUser };
--- src/view/parentPanel.js
+++ src/view/parentPanel.js
@@ -1,49 +1,23 @@
-import { addUserPost } from '../firebase.js';
+import { addUserPost, showPostUser } from '../firebase.js';
 
 export default () => {
   const root = document.getElementById('root')
   const viewParentPanel = `
-  <h1>PANEL DE PADRES</1>
-  <div class="post">
-  <textarea class="comment-box" placeholder="Escribe tu tema a tratar" id="post"> </textarea>
-  <button class="button-add" id="btnPost">Agregar</button>
-  </div>
-  <div id="postNew"> Aqui van los postNew
+  <h1 class="parentPanel">PANEL DE PADRES</1>
+  <textarea class="box-add-new-post" placeholder="Escribe tu tema a tratar" id="addNewPost"></textarea>
+  <button class="button-add" id="btnAddPost">Agregar</button>
+  <div id="allNewPost" class="allNewPost">
   </div>
     `
   const divElement = document.createElement('div')
   divElement.innerHTML = viewParentPanel;
   root.appendChild(divElement);
 
-  const buttonAddPost = document.querySelector('#btnPost')
+  showPostUser();
+
+  const buttonAddPost = document.querySelector('#btnAddPost')
   buttonAddPost.addEventListener('click', addUserPost)
 
 
   return divElement;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-<textarea class="comment-box" placeholder="Escribe tu comentario" id="comment"> </textarea>
-  <button class="comments" id="btnComment">Comentar</button>
-  <p> 3 </p> <img class="logo-likes" src="img/like.svg">
-  <div class="container__box>
-    <div class="container__box--postTimeLine">
-      <div class="container__box--answer">
-        <div class="container__box--comment">
-        </div
-      </div>
-    </div>
-  </div>*/
