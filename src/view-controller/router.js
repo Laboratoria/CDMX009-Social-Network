f44d23455f126f3f6dd96e32384a4a03496387dc
vanyaxel <vanya_xel@live.com.mx>
2020-04-27 12:46:12 -0500

    package

--- src/view-controller/router.js
+++ src/view-controller/router.js
@@ -1,26 +1,26 @@
 import { components } from '../view/index.js';
 
 export const changeTmp = (hash) => {
-    const id = hash.split('/')[1];
-    const sectionMain = document.getElementById('contentCreated');
-    sectionMain.innerHTML = '';
+  const id = hash.split('/')[1];
+  const sectionMain = document.getElementById('contentCreated');
+  sectionMain.innerHTML = '';
 
-    switch (hash) {
-        case '':
-        case '#':
-        case '#/':
-        case '#/login':
-            { return sectionMain.appendChild(components.login()); }
-        case '#/logup':
-        case '#/profile':
-        case '#/home':
-        case '#/reviews':
-        case '#/saved':
-        case '#/followers':
-        case '#/follow':
-            
-            { return sectionMain.appendChild(components[id]()); }
-        default:
-            return sectionMain.appendChild(components.different())
-    }
+  switch (hash) {
+    case '':
+    case '#':
+    case '#/':
+    case '#/login':
+    { return sectionMain.appendChild(components.login()); }
+    case '#/logup':
+    case '#/profile':
+    case '#/home':
+    case '#/reviews':
+    case '#/saved':
+    case '#/followers':
+    case '#/follow':
+
+    { return sectionMain.appendChild(components[id]()); }
+    default:
+      return sectionMain.appendChild(components.different());
+  }
 };
