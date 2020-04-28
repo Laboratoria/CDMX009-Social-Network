681565db268b22aa83b36313ebf294b9ece3357e
9JazzCodeLBM <laura.bailon09@gmail.com>
2020-04-28 08:56:34 -0500

    Working with testing

--- src/auth.test.js
+++ src/auth.test.js
@@ -7,3 +7,18 @@ test('emailLogin', () => {
     expect(auth.emailLogin(email, password)).toBe(false);
 });
 
+// esperamos que si escribe un correo inválido, le devuelva un error
+
+test('emailLogin', () => {
+    let email = "antropologia@gmail"
+    let password = "123DZM"
+    expect(auth.emailLogin(email, password)).toThrowError('Email inválido');
+});
+
+/* esperamos que si su contraseña no coincide con la guardada en db, le devuelva un error
+
+test('emailLogin', () => {
+    let email = ""
+    let password = "123D"
+    expect(auth.logout(email, password))..toThrowError('Contraseña incorrecta');
+});
