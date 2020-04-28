2df30e3fb38a4b39f1199b48b2cd9df58c99e14b
JimenaMV <jimenamanzanares.v@gmail.com>
2020-04-28 18:04:44 -0500

    Lista vista principal y estilos css

--- src/views/principal.js
+++ src/views/principal.js
@@ -1,10 +1,19 @@
 export default () => {
     const principal = `
+    
+    <div class="slider">
+    <ul>
+         <li><img src="/img/slider1.png" alt=""></li>
+         <li><img src="/img/slider2.png" alt=""></li>
+         <li><img src="/img/slider3.png" alt=""></li>
+    </ul>
+
+    </div>
         <div class="welcome cover">
             <div class="welcome-info">
-                <h1 class="title has-text-centered has-text-grey-lighter title is-3" id='welcome-title' >Microcuentos</h1>
-                <h2 class="title has-text-centered has-text-grey-lighter title is-4" >ilustrados</h2>
-                <p class="title has-text-centered has-text-grey-lighter title is-6" id='instructions'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit magnam sunt eligendi nihil odit cupiditate autem ipsa nulla! Quaerat, iste.</p>
+                <h1 class="title has-text-centered has-text-warning title is-3" id='welcome-title'>Microcuentos</h1>
+                <h2 class="title has-text-centered has-text-warning title is-4" id='welcome-subtitle'>ilustrados</h2>
+                <p class="title has-text-centered has-text-warning title is-6" id='instructions'><em>*Escribe un microcuento*<br>*Ilustra un microcuento*<br>++Escribe e ilustra++</em></p>
                 <div class="img-enter centerItem">
                     <figure>
                         <img src="/img/ojo.png" id="goLogin" alt="">
@@ -12,15 +21,9 @@ export default () => {
                 </div> 
             </div>
         </div>
-
-        <div class="slider">
-        <ul>
-             <li><img src="/img/slider1.png" alt=""></li>
-             <li><img src="/img/slider2.png" alt=""></li>
-             <li><img src="/img/slider3.png" alt=""></li>
-        </ul>
-        </div>
     `
+
+    
     // <div class="welcome cover">
     //         <div class="welcome-info">
     //             <h1 class="title has-text-centered has-text-grey-lighter title is-3" id='welcome-title' >Microcuentos</h1>
