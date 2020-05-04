5e0731ab767e61bbaea861177701e8a74f3cf7d8
Nallely <nallely232@hotmail.com>
2020-05-03 21:32:54 -0500

    Se corrigio función del feed y se añadio función de escucha de los elementos en el post

--- src/database.js
+++ src/database.js
@@ -1,18 +1,19 @@
 /* eslint-disable */
 let authError;
+let feedData;
 
 const database = {
-  signUp: (regEmail, regPassword, firebasePipo=null) => {
-    (firebasePipo? firebasePipo: firebase).auth().createUserWithEmailAndPassword(regEmail, regPassword)
+  signUp: (regEmail, regPassword, firebasePipo = null) => {
+    console.log(regEmail);
+    console.log(regPassword);
+    (firebasePipo ? firebasePipo : firebase).auth().createUserWithEmailAndPassword(regEmail, regPassword)
       .catch((error) => {
         authError = error;
         return authError;
       });
   },
-  signIn: () => {
-    const logEmail = document.getElementById('logEmail').value;
-    const logPassword = document.getElementById('logPassword').value;
-    firebase.auth().signInWithEmailAndPassword(logEmail, logPassword)
+  signIn: (logEmail, logPassword, firebasePipo = null) => {
+    (firebasePipo ? firebasePipo : firebase).auth().signInWithEmailAndPassword(logEmail, logPassword)
       .catch((error) => {
         authError = error;
         return authError;
@@ -22,7 +23,8 @@ const database = {
     const errorMsg = authError;
     return errorMsg;
   },
-  signInGoogle: () => {
+  signInGoogle: () => {    
+  const providerGoogle = new firebase.auth.GoogleAuthProvider();
     firebase.auth().signInWithPopup(providerGoogle)
       .then((result) => {
         const user = result.user;
@@ -32,7 +34,8 @@ const database = {
       });
   },
   signInFacebook: () => {
-    firebase.auth().signInWithPopup(providerFb).then((result) => {
+    const providerFb = new firebase.auth.FacebookAuthProvider();
+    firebase.auth().signInWithPopup(providerFb).then((result) => {      
       const token = result.credential.accessToken;
       console.log(token);
       const user = result.user;
@@ -41,57 +44,36 @@ const database = {
       throw error('¡Error!');
     });
   },
-  getPostFeed: async () => { 
+  getFeedData: async (renderFunction) => {
+    const imageRefPost = firebase.database().ref().child('post-image');
     imageRefPost.on('value', async (snapshot) => {
-      const data = snapshot.val();
-      let result = '';
-      for (const key in data) {
-        if (Object.prototype.hasOwnProperty.call(data, key)) {
-          const timeStamp = data[key].postTime;
-          const normalDate = new Date(timeStamp);
-          const dateFormat = normalDate.toLocaleString();
-          const userId = data[key].uid;
-          let userName = 'default';
-          let photoUser = '';
-          const usersRef = firebase.database().ref('users');
-          await usersRef.child(userId).once('value', async (snapshots) => {
-            userName = snapshots.val();
-            const imageUser = firebase.database().ref('image');
+      feedData = snapshot.val();
+      Object.keys(feedData).reverse().forEach((key) => {
+        const timeStamp = feedData[key].postTime;
+        const normalDate = new Date(timeStamp);
+        const dateFormat = normalDate.toLocaleString();
+        const userId = feedData[key].uid;
+        const picURL = feedData[key].url;
+        const picCaption = feedData[key].comment;
+        let userName = 'default';
+        let photoUser = '';
+        const usersRef = firebase.database().ref('users');
+        usersRef.child(userId).once('value', async (snapshots) => {
+          userName = snapshots.val();
+          const imageUser = firebase.database().ref('image');
             await imageUser.child(userId).once('value', (snap) => {
-              photoUser = snap.val();
-            });
-            result += `
-              <div class="userInfo media">
-                <div class="image is-48x48">
-                  <img src=${photoUser.url} class="is-rounded"/>
-                </div>
-                <div class="media-content">
-                  <p>${'@'}${userName.userName}</p>
-                </div>
-              </div>
-              <div class="file is-centered">
-                <img src='${data[key].url}'/>
-              </div>
-              <div>
-                <img id="like" width="5%" height="5%" src="https://i.ibb.co/Kqxbg7Y/smile-rgb.png"/>
-                <img id="dislike" width="5%" height="5%" src="https://i.ibb.co/0GdLWZ6/kk-rgb.png"/>
-                <img id="commentPost" width="5%" height="5%" src="https://i.ibb.co/c20jsVj/coment-rgb.png"/>
-                <p>${data[key].comment}</p>
-                <p>${dateFormat}</p>
-              </div>
-              </br>
-            `;
-            document.getElementById('postFeed').innerHTML = result;
+            photoUser = snap.val();
           });
-        }
-      }
-    })
-  }, 
-  userObserver: (renderFeed) => { 
-    firebase.auth().onAuthStateChanged((user) => { 
+          renderFunction(photoUser, userName, picURL, picCaption, dateFormat);
+        });
+      });
+    });
+  },
+  userObserver: (home) => {
+    firebase.auth().onAuthStateChanged(async (user) => {
       if (user) {
         console.log('existe usuario activo');
-        renderFeed();
+        await home();
         console.log('*****************');
         console.log(user.emailVerified);
         console.log('*****************');
@@ -111,6 +93,7 @@ const database = {
       .then(doc => doc.data());
   },
   getPostPic: () => {
+    const imageRefPost = firebase.database().ref().child('post-image');
     imageRefPost.on('value', (snapshot) => {
       const data = snapshot.val();
       let result = '';
@@ -121,6 +104,7 @@ const database = {
     });
   },
   uploadPicture: (file) => {
+    const storage = firebase.storage().ref();
     const uploadTask = storage.child(`profilePictures/${file.name}`).put(file);
     uploadTask.on('state_changed', (snapshot) => {
       const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
@@ -134,19 +118,19 @@ const database = {
           break;
         default:
       }
-    
     }, (error) => {
       console.error(error);
     },
-    () => {
-      uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
-        console.log('File available at', downloadURL);
-        database.createNodeFirebase(file.name, downloadURL);
-        database.getProfilePic();
+      () => {
+        uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
+          console.log('File available at', downloadURL);
+          database.createNodeFirebase(file.name, downloadURL);
+          database.getProfilePic();
+        });
       });
-    });
   },
   uploadPicturePost: (file) => {
+    const storage = firebase.storage().ref();
     const uploadImg = document.getElementById('uploadImg').files[0];
     const uploadTask = storage.child(`postImage/${uploadImg.name}`).put(uploadImg);
     uploadTask.on('state_changed', (snapshot) => {
@@ -171,6 +155,7 @@ const database = {
     });
   },
   createNodeFirebase: (nameImage, url) => {
+    const db = firebase.firestore();
     const userPhotoProf = { name: nameImage, url, uid: firebase.auth().currentUser.uid };
     console.log(userPhotoProf);
     firebase.database().ref(`image/${userPhotoProf.uid}`).set(userPhotoProf);
@@ -181,12 +166,14 @@ const database = {
     });
   },
   createNodeFirebaseForPost: (nameImage, url) => {
+    const db = firebase.firestore();
     const postMessage = document.getElementById('postMessage').value;
     const userImgePost = {
       name: nameImage, url, uid: firebase.auth().currentUser.uid, postTime: firebase.database.ServerValue.TIMESTAMP, comment: postMessage,
     };
     firebase.database().ref('post-image')
       .push(userImgePost);
+    // console.log(userImgePost.key)
     db.collection('post-image').add({
       name: nameImage,
       url,
@@ -196,6 +183,7 @@ const database = {
     });
   },
   saveData: (user) => {
+    const db = firebase.firestore();
     const userName = document.getElementById('userName').value;
     const profileName = document.getElementById('profileName').value;
     const biography = document.getElementById('biography').value;
@@ -209,6 +197,17 @@ const database = {
       profileName,
       biography,
     });
+  },
+  deletePost: (key) => {
+    let postDelet = firebase.database().ref('post-image/'+ key)
+    postDelet.remove();
+    
+    // const db = firebase.firestore();
+    // db.collection("post-image").doc(key).delete().then(function () {
+    //   console.log(key);
+    // }).catch(function (error) {
+    //   console.error("Error removing document: ", error);
+    // });
   },
   logout: () => {
     firebase.auth().signOut().then(() => {
--- test/example.spec.js
+++ test/example.spec.js
@@ -1,21 +1,20 @@
 /* eslint-disable */
 import * as firebase from 'firebase';
 import 'firebase/auth';
-import 'firebase/database';
-import 'firebase/storage';
-import 'firebase/firestore';
 import database from '../src/database.js';
 
-//config
+const firebaseConfig = {
+  apiKey: 'AIzaSyD6boxRV32Ld50HrVuWx2Fkl5ZlYQyAabk',
+  authDomain: 'memingos-abeea.firebaseapp.com',
+  databaseURL: 'https://memingos-abeea.firebaseio.com',
+  projectId: 'memingos-abeea',
+  storageBucket: 'memingos-abeea.appspot.com',
+  messagingSenderId: '299345545979',
+  appId: '1:299345545979:web:a1d0095b63dc2c48e5be5e',
+  measurementId: 'G-7SHPQ7YX96',
+};
 
 firebase.initializeApp(firebaseConfig);
-const providerGoogle = new firebase.auth.GoogleAuthProvider();
-const providerFb = new firebase.auth.FacebookAuthProvider();
-const db = firebase.firestore();
-const storage = firebase.storage().ref();
-const usersRef = firebase.database().ref('users');
-const imageRefPost = firebase.database().ref().child('post-image');
-const imageUser = firebase.database().ref('image');
 
 describe('database', () => {
   it('should be a object', () => {
@@ -32,7 +31,7 @@ describe('database.signUp', () => {
     firebase.auth().onAuthStateChanged(function(user) { 
         expect(typeof user).toBe('object');
     });    
-    database.signUp ("pruebachida@yopmail.com", "lapapasabemejorcocida", firebase)
+    database.signUp ("pruebachida@yopmail.com", "lapapasabemejorcocida",firebase)
   });
 });
 
